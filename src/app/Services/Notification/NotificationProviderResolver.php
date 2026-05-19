c75c3a8aaf2d985128593529aabfbd8264ea032c
it-dude-ru <it-dude@ya.ru>
2026-05-19 10:34:00 +0300

    Рефакторинг архитектуры - провайдеры уведомлений отделены от инфраструктурных enum

--- src/app/Services/Notification/NotificationProviderResolver.php
+++ src/app/Services/Notification/NotificationProviderResolver.php
@@ -4,11 +4,18 @@ namespace App\Services\Notification;
 
 use App\Contracts\NotificationProviderInterface;
 use App\Enums\NotificationChannel;
+use App\Services\Notification\Providers\EmailProviderMock;
+use App\Services\Notification\Providers\SmsProviderMock;
 
 class NotificationProviderResolver
 {
     public function resolve(NotificationChannel $channel): NotificationProviderInterface
     {
-        return app($channel->providerClass());
+        $class = match ($channel) {
+            NotificationChannel::Sms   => SmsProviderMock::class,
+            NotificationChannel::Email => EmailProviderMock::class,
+        };
+
+        return app($class);
     }
 }
